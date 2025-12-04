0ba9804113cd111d708ac05a5c239701c4bb8c9b
Steve <125539+steve7411@users.noreply.github.com>
2025-12-04 17:51:38 -0500

    Rewriting day 4 solutions

--- Advent2025/Day04/Day04.cs
+++ Advent2025/Day04/Day04.cs
@@ -1,88 +1,110 @@
-﻿using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.X86;
 
 namespace Advent2025.Day04;
 
 internal sealed class Day04 : DayBase {
+    const uint NINE_BITS = 0x1FF;
+
+    [InlineArray(139 << 2)]
+    private struct Buffer { public ulong first; }
+
     private readonly int width;
     private readonly int height;
-    private readonly byte[,] grid;
+    private readonly int elemsPerRow;
+    private Buffer grid;
 
+    [SkipLocalsInit]
     public Day04() {
-        Span<byte> buffer = stackalloc byte[138];
+        Span<char> newlineDump = stackalloc char[Environment.NewLine.Length];
 
         using var reader = GetDataReader();
         (width, height) = reader.BaseStream.GetLineInfoForRegularFile();
-        grid = new byte[height, width];
-        Span<byte> gridSpan = MemoryMarshal.CreateSpan(ref grid[0, 0], grid.Length);
-        for(var idx = 0; !reader.EndOfStream; idx += width)
-            reader.ReadLine(gridSpan[idx..]);
-        gridSpan.RightShift(6);
+        elemsPerRow = width + 64 >>> 6;
+
+        for (var rowOffset = 0; !reader.EndOfStream; rowOffset += 4) {
+            var rem = width + 1;
+            for (var i = 0; i < elemsPerRow; ++i, rem -= 64) {
+                var end = Math.Min(64, rem);
+                ref var bits = ref grid[rowOffset + i];
+                for (var x = i == 0 ? 1 : 0; x < end; ++x)
+                    bits |= (ulong)(reader.Read() >>> 6) << x;
+            }
+            reader.Read(newlineDump);
+        }
     }
 
-    private uint GetNeighborCount(int x, int y) {
-        var res = 0U;
-        var cell = grid[y, x];
-        if (cell == 0)
-            return 8;
-        var (minX, maxX) = (Math.Max(0, x - 1), Math.Min(width - 1, x + 1));
-        var (minY, maxY) = (Math.Max(0, y - 1), Math.Min(height - 1, y + 1));
-        var rowMask = (1U << (maxX - minX + 1 << 3)) - 1;
-        for (var i = minY; i <= maxY; ++i) {
-            var bits = Unsafe.As<byte, uint>(ref grid[i, minX]) & rowMask;
-            res += Popcnt.PopCount(bits);
+    private uint CountRemovable() {
+        var count = 0U;
+        Span<uint> neighborhoods = stackalloc uint[width];
+        for (var y = 0; y <= height; ++y) {
+            var rowOffset = y << 2;
+            var rem = width;
+            for (var i = 0; i < elemsPerRow; ++i, rem -= 64) {
+                var bits = grid[rowOffset + i];
+                var idx = i << 6;
+                var end = Math.Min(2, rem);
+                for (int j = 0, k = 0; j < 2; ++j) {
+                    for (; k < end; ++k, ++idx, bits >>>= 1) {
+                        ref var relevant = ref neighborhoods[idx];
+                        relevant = relevant << 3 & NINE_BITS | (uint)bits & 7;
+                        count += Popcnt.PopCount(relevant & ~0x10U) < 4 ? relevant >>> 4 & (uint)(-y >>> 31) : 0;
+                    }
+                    bits |= grid[rowOffset + i + 1] << 62;
+                    end = Math.Min(64, rem);
+                }
+            }
         }
-
-        var final = res - cell;
-        return final;
+        return count;
     }
 
-    private uint RemoveIfPossible(int x, int y) {
-        var res = 0U;
-        ref var cell = ref grid[y, x];
-        if (cell == 0)
-            return 0;
-        var (minX, maxX) = (Math.Max(0, x - 1), Math.Min(width - 1, x + 1));
-        var (minY, maxY) = (Math.Max(0, y - 1), Math.Min(height - 1, y + 1));
-        var rowMask = (1U << (maxX - minX + 1 << 3)) - 1;
-        for (var i = minY; i <= maxY; ++i) {
-            var bits = Unsafe.As<byte, uint>(ref grid[i, minX]) & rowMask;
-            res += Popcnt.PopCount(bits);
-        }
+    [SkipLocalsInit]
+    private uint RemoveAllPossible() {
+        var removeCount = 0U;
+        Span<uint> buffer = stackalloc uint[width + 2];
+        var neighborhoods = buffer[1..^1];
 
-        var final = res - cell;
-        if (final < 4) {
-            cell = 0;
-            return 1;
+        for (var count = 1U; count != 0;) {
+            neighborhoods.Clear();
+            count = 0;
+            for (var y = 0; y <= height; ++y) {
+                var rowOffset = y << 2;
+                var rem = width;
+                for (var i = 0; i < elemsPerRow; ++i, rem -= 64) {
+                    var bits = grid[rowOffset + i];
+                    var idx = i << 6;
+                    var end = Math.Min(2, rem);
+                    for (int j = 0, k = 1; j < 2; ++j) {
+                        for (; k <= end; ++k, ++idx, bits >>>= 1) {
+                            ref var relevant = ref neighborhoods[idx];
+                            relevant = relevant << 3 & NINE_BITS | (uint)bits & 7;
+                            if (Popcnt.PopCount(relevant & ~0x10U) < 4 & (relevant & 16) != 0 & y > 0) {
+                                ++count;
+                                Unsafe.Add(ref relevant, -1) ^= 32;
+                                relevant ^= 16;
+                                Unsafe.Add(ref relevant, 1) ^= 1;
+                                var gridOffset = k >>> 6;
+                                grid[rowOffset - 4 + i + gridOffset] ^= 1UL << k;
+                            }
+                        }
+                        bits |= grid[rowOffset + i + 1] << 62;
+                        end = Math.Min(64, rem);
+                    }
+                }
+            }
+            removeCount += count;
         }
-        return 0;
+        return removeCount;
     }
 
     public override object? Part1() {
-        var count = 0U;
-        for (var y = 0; y < height; ++y) {
-            for (var x = 0; x < width; ++x)
-                count += GetNeighborCount(x, y) < 4 ? 1U : 0;
-        }
-
+        var count = CountRemovable();
         Print("The number of accessible paper rolls is: {0}", count);
         return Box<uint>.Instance(count);
     }
 
-
     public override object? Part2() {
-        var removeCount = 0U;
-        for (var count = 1U; count != 0;) {
-            count = 0;
-            for (var y = 0; y < height; ++y) {
-                for (var x = 0; x < width; ++x)
-                    count += RemoveIfPossible(x, y);
-            }
-            removeCount += count;
-        }
-
+        var removeCount = RemoveAllPossible();
         Print("The number of accessible paper rolls after all rounds: {0}", removeCount);
         return Box<uint>.Instance(removeCount);
     }
